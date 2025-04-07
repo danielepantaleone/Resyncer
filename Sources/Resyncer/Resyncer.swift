29907ff30655407c107a45d3a85fee50c54ece2e
Daniele Pantaleone <danielepantaleone@me.com>
2025-04-07 09:47:49 +0200

    Improved swift documentation and swift file headers

--- Sources/Resyncer/Resyncer.swift
+++ Sources/Resyncer/Resyncer.swift
@@ -2,16 +2,34 @@
 //  Resyncer.swift
 //  Resyncer
 //
-//  Created by Daniele Pantaleone
-//    - Github: https://github.com/danielepantaleone
-//    - LinkedIn: https://www.linkedin.com/in/danielepantaleone
+//  GitHub Repo and Documentation: https://github.com/danielepantaleone/Resyncer
 //
-//  Copyright © 2024 Daniele Pantaleone. Licensed under MIT License.
+//  Copyright © 2024 Daniele Pantaleone. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
 
-/// Resyncer enables you to call asynchronous code within a synchronous environment by pausing the current thread until the asynchronous task completes.
+/// Resyncer enables you to call asynchronous code within a synchronous environment by pausing the current thread
+/// until the asynchronous task completes. It achieves this by offloading the asynchronous work to a separate thread,
+/// either using an `OperationQueue` or leveraging Swift concurrency with `Task`.
 public final class Resyncer {
     
     // MARK: - Properties
